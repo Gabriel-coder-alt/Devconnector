fd4e6200b06b01ebff29eecceeb079ba1d7fee3a
Gabriel <gabrielejenobo95@gmail.com>
2021-08-08 01:36:51 +0200

    Modified the Profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,8 +6,8 @@ import { getProfileById } from '../../actions/profile';
 import Spinner from '../dashboard/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
-import ProfileExperience from './ProfileExperience';
-import ProfileEducation from './ProfileEducation';
+// import ProfileExperience from './ProfileExperience';
+// import ProfileEducation from './ProfileEducation';
 import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ profile: { profile, loading }, auth, match, getProfileById  }) => {
@@ -29,27 +29,6 @@ const Profile = ({ profile: { profile, loading }, auth, match, getProfileById  }
                 <div className="profile-grid my-1">
                     <ProfileTop profile={profile} />
                     <ProfileAbout profile={profile} />
-                    <div class="profile-exp bg-white p-2">
-                        <h2 class="text-primary">Experience</h2>
-                        {profile.experiences.length > 0 ? (
-                            <Fragment>
-                                {profile.experiences.map( experience => (
-                                    <ProfileExperience key={experience._id} experience={experience} />
-                                ))}
-                            </Fragment>
-                        ) : (<h4>No experience credentials</h4>)}
-                    </div>
-
-                    <div class="profile-edu bg-white p-2">
-                        <h2 class="text-primary">Education</h2>
-                        {profile.education.length > 0 ? (
-                            <Fragment>
-                                {profile.education.map( edu => (
-                                    <ProfileEducation key={edu._id} education={edu} />
-                                ))}
-                            </Fragment>
-                        ) : (<h4>No education credentials</h4>)}
-                    </div>
                     {profile.githubusername && <ProfileGithub username={profile.githubusername} />}
                 </div>
             </Fragment>

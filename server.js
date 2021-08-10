648de54b6a7b2375a9a441f1e0d4f2636e2b640c
Gabriel <gabrielejenobo95@gmail.com>
2021-08-10 22:17:42 +0200

    Edited the server file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     
     // Set static folder
-    app.use('/static', express.static('./client/build'));
+    app.use(express.static(path.join(__dirname, './client/build')));
 
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));

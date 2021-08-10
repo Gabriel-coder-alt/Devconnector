577c0f263e0eb232982c07a5159a31fa5f47af95
Gabriel <gabrielejenobo95@gmail.com>
2021-08-10 13:35:54 +0200

    I made changes to the server.js file

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     
     // Set static folder
-    app.use('/static', express.static('client/build'));
+    app.use('/static', express.static('./client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
     });
 }
 

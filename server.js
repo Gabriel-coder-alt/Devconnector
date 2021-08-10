d61f4af2ee4ed4ea3b8429106f99dcfe84a67025
Gabriel <gabrielejenobo95@gmail.com>
2021-08-11 00:01:27 +0200

    I edited the server file

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ connectDB();
 
 // Init Middleware
 app.use(express.json());
-  
-// Set static folder
-app.use(express.static(path.resolve(__dirname, './client/build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    
+    // Set static folder
+    app.use('/static', express.static('./client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    });
+}
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
